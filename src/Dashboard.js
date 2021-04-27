ad5a1325d987ca6c53ddec3a371246d02e306bb2
fullstackdev215 <genius.developer.63@gmail.com>
2021-04-27 11:57:28 -0700

    feat: fix cursor

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -45,10 +45,12 @@ function Dashboard({account, balance}) {
       top: '24px',
       right: theme.spacing(12),
     },
+    Btn: {
+      cursor: 'pointer'
+    },
     topText: {
       fontWeight: '700',
       fontSize: '14px',
-      cursor: 'pointer',
       display: 'inline-flex',
       width: 'fit-content',
       marginRight: '12px',
@@ -68,7 +70,7 @@ function Dashboard({account, balance}) {
             <Grid className={classes.topBtn}>
               <Grid container direction="column">
                 <Grid container direction="row" alignItems="center" justify="flex-end">
-                  <Typography className={classes.topText}>Home</Typography>
+                  <Typography className={`${classes.topText} ${classes.Btn}`}>Home</Typography>
                   <Button onClick={connectWallet} variant="contained" color="primary" className={classes.button}>
                     Connect wallet
                   </Button>
